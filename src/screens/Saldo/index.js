6adb620897f36cd11e93d46d540cd50cfa3501b0
mateodelat <mateodelat@gmail.com>
2021-12-17 15:27:03 -0600

    Saldo corregido para ver payouts

--- src/screens/Saldo/index.js
+++ src/screens/Saldo/index.js
@@ -2,7 +2,10 @@ import React, { useEffect, useState } from 'react'
 import { Alert, Image, ImageBackground, Pressable, RefreshControl, ScrollView, StyleSheet, Text, View } from 'react-native'
 import { container, formatAMPM, formatCuentaCLABE, formatDateShort, formatMoney, getUserSub, moradoOscuro } from '../../../assets/constants'
 import { Loading } from '../../components/Loading';
-import { Feather } from '@expo/vector-icons';
+
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+
 import { DataStore } from '@aws-amplify/datastore';
 import { Usuario } from '../../models';
 import { Reserva } from '../../models';
@@ -13,11 +16,19 @@ export default function () {
     const [refreshing, setRefreshing] = useState(false);
 
     const [usuarioActual, setUsuarioActual] = useState({});
+    const [payouts, setPayouts] = useState(false);
+
+    const [pagosAlBanco, setPagosAlBanco] = useState(null);
+
 
     useEffect(() => {
-        fetchData()
         getActualUser()
+            .then(usr => {
+                fetchPayments(usr)
+                fetchPayouts(usr)
 
+
+            })
     }, []);
 
     async function getActualUser() {
@@ -26,26 +37,26 @@ export default function () {
         const usuario = await DataStore.query(Usuario, sub)
 
         setUsuarioActual(usuario)
-
+        return usuario
 
     }
 
 
-    function fetchData() {
-        return fetch("https://api.stripe.com/v1/balance_transactions", {
-            method: "GET",
-            headers: {
-                "Content-Type": "multipart/form-data",
-                "Accept": "application/json",
-                "Authorization": "Bearer " + "rk_test_51J7OwUFIERW56TAE2Af9n7pLq9RrCk5ABDR6EoPFdHzPIM9v3kkxCAt12wdBIvV6b9QKLxMlsPlkK2fmX9SO1MtX00H16Ba1De",
-                "Stripe-Account": "acct_1Jzmuz2V1EJMOgyd"
-            }
-        }).then(r => r.json())
+    function fetchPayments(user) {
+        return fetch("https://api.stripe.com/v1/balance_transactions?"
+            + "limit=100"
+            + "&&type=payment"
+            //  + "&&starting_after=txn_1K6Mum2V1EJMOgydz9hCwNxA"
+            , {
+                method: "get",
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Accept": "application/json",
+                    "Authorization": "Bearer " + "rk_test_51J7OwUFIERW56TAE2Af9n7pLq9RrCk5ABDR6EoPFdHzPIM9v3kkxCAt12wdBIvV6b9QKLxMlsPlkK2fmX9SO1MtX00H16Ba1De",
+                    "Stripe-Account": user.stripeID,
+                },
+            }).then(r => r.json())
             .then(r => {
-                console.log(r)
-                if (r.has_more) {
-                    // Alert.alert("Faltan", "Faltan transferencias a mostrar, avisa al desarrollador")
-                }
                 if (r.error) {
                     Alert.alert("Error", "Ocurrio un error obteniendo las transferencias")
                     setTransacciones([])
@@ -64,13 +75,51 @@ export default function () {
             })
     }
 
+    function fetchPayouts(user) {
+        return fetch("https://api.stripe.com/v1/balance_transactions?"
+            + "limit=100"
+            + "&&type=payout"
+            //  + "&&starting_after=txn_1K6Mum2V1EJMOgydz9hCwNxA"
+            , {
+                method: "get",
+                headers: {
+                    "Content-Type": "multipart/form-data",
+                    "Accept": "application/json",
+                    "Authorization": "Bearer " + "rk_test_51J7OwUFIERW56TAE2Af9n7pLq9RrCk5ABDR6EoPFdHzPIM9v3kkxCAt12wdBIvV6b9QKLxMlsPlkK2fmX9SO1MtX00H16Ba1De",
+                    "Stripe-Account": user.stripeID,
+                },
+            }).then(r => r.json())
+            .then(r => {
+                if (r.has_more) {
+                    console.log("Faltan transferencias a mostrar, avisa al desarrollador")
+                }
+                if (r.error) {
+                    Alert.alert("Error", "Ocurrio un error obteniendo las transferencias")
+                    setTransacciones([])
+                    return
+                }
+                r = r.data
+                const transferencias = r?.map(e => ({
+                    status: e.status,
+                    amount: (e.net / 100) - (e.net / 100) * 2,
+                    creado: new Date(e.created * 1000)
+                })).sort((a, b) => a.creado < b.creado)
+
+
+                setPagosAlBanco(transferencias)
+                return
+            })
+    }
+
     function formatDate(input) {
-        return formatDateShort(input) + " " + formatAMPM(input)
+        return formatDateShort(input) + " " + formatAMPM(input, false, true)
     }
 
     async function onRefresh() {
         setRefreshing(true)
-        await fetchData()
+        fetchPayments(usuarioActual)
+        fetchPayouts(usuarioActual)
+
         setRefreshing(false)
 
     }
@@ -81,8 +130,14 @@ export default function () {
         if (e.status === "pending") {
             saldo += e.amount
         }
-
     })
+
+    let saldoEnviado = 0
+    pagosAlBanco?.map(e => {
+        saldoEnviado += e.amount
+    })
+
+
 
 
     return (
@@ -105,8 +160,8 @@ export default function () {
                 <ImageBackground style={styles.card} source={require("../../../assets/IMG/Card.png")}>
 
                     <View style={styles.insideCard}>
-                        <Text style={styles.balanceTxt}>Saldo por enviar a cuenta</Text>
-                        <Text style={styles.numberBalance}>{formatMoney(saldo)}</Text>
+                        <Text style={styles.balanceTxt}>{!payouts ? "Saldo por enviar a banco" : "Enviado al banco"}</Text>
+                        <Text style={styles.numberBalance}>{formatMoney(payouts ? saldoEnviado : saldo)}</Text>
 
                         <View style={{ flex: 1, }} />
 
@@ -114,38 +169,72 @@ export default function () {
 
                     </View>
                 </ImageBackground>
-                <Text style={styles.aviso}>El pago tarda 7 dias en aparecer en tu cuenta bancaria</Text>
+                <View style={styles.iconsContainer}>
+
+                    <MaterialCommunityIcons
+                        onPress={() => setPayouts(false)}
+
+                        style={{
+                            marginRight: 45,
+                        }}
+                        name="send-circle"
+                        size={45}
+                        color={moradoOscuro + (payouts ? "44" : "ff")}
+                    />
+
+                    <Pressable
+                        onPress={() => setPayouts(true)}
+                        style={{
+                            padding: 4,
+                            width: 38,
+                            height: 38,
+                            backgroundColor: moradoOscuro + (!payouts ? "44" : "ff"),
+                            borderRadius: 45,
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <FontAwesome5
+                            name="hand-holding-usd"
+                            size={20}
+                            color="#fff"
+                        />
+                    </Pressable>
+                </View>
+
+                <Text style={styles.aviso}>El pago tarda 7 dias en enviarse a tu cuenta bancaria</Text>
+
 
                 <View style={styles.transaccionesContainer}>
-                    <Text style={styles.transaccionesTitle}>Pagos</Text>
+                    <Text style={styles.transaccionesTitle}>{payouts ? "Enviado al banco" : "Pagos de reservaciones"}</Text>
 
-                    {!transacciones ?
+                    {!transacciones || !pagosAlBanco ?
                         <Loading indicator /> :
-                        transacciones.map((e, i) => (
-                            <Pressable
-                                onPress={() => Alert.alert("Navgar a detalle de fecha")}
-                                key={i.toString()}
-                                style={styles.transaccion}>
+                        (!payouts ? (transacciones) : pagosAlBanco)
+                            .map((e, i) => (
+                                <Pressable
+                                    key={i.toString()}
+                                    style={styles.transaccion}>
 
-                                {/* <Image source={require("../../../assets/IMG/Persona.png")} style={styles.profileImage} /> */}
+                                    {/* <Image source={require("../../../assets/IMG/Persona.png")} style={styles.profileImage} /> */}
 
-                                <View style={styles.dataPerson}>
-                                    <View>
-                                        {/* <Text style={styles.nickname}>@mateo delat</Text> */}
-                                        <Text style={styles.transaccionDate}>{formatDate(e.creado)}</Text>
+                                    <View style={styles.dataPerson}>
+                                        <View>
+                                            {/* <Text style={styles.nickname}>@mateo delat</Text> */}
+                                            <Text style={styles.transaccionDate}>{formatDate(e.creado)}</Text>
+
+                                        </View>
+
+                                        <View style={{ alignItems: 'flex-end' }}>
+                                            <Text style={styles.monney}>{formatMoney(e.amount)}</Text>
+                                            <Text style={[styles.status, { color: e.status === "pending" ? "orange" : "green", }]}>{e.status === "pending" ? "en camino" : "depositado"}</Text>
+
+                                        </View>
 
                                     </View>
+                                </Pressable>
 
-                                    <View style={{ alignItems: 'flex-end' }}>
-                                        <Text style={styles.monney}>{formatMoney(e.amount)}</Text>
-                                        <Text style={[styles.status, { color: e.status === "pending" ? "orange" : "green", }]}>{e.status === "pending" ? "en camino" : "depositado"}</Text>
-
-                                    </View>
-
-                                </View>
-                            </Pressable>
-
-                        ))
+                            ))
                     }
                 </View>
             </ScrollView>
@@ -190,7 +279,7 @@ const styles = StyleSheet.create({
     },
 
     aviso: {
-        marginTop: 30,
+        marginTop: 10,
         fontSize: 16,
         color: '#999',
         textAlign: 'center',
@@ -240,6 +329,14 @@ const styles = StyleSheet.create({
     status: {
         color: "orange",
         marginTop: 5,
-    }
+    },
+
+    iconsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 30,
+    },
+
 
 })
